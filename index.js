2c8a28455c73f2b3c034d9f199b724bbe187968a
Shoaib Khan <mshoaibkhan91@gmail.com>
2017-12-28 12:31:44 +0100

    template added

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
+/**
+ * Main script that starts the server and serve the urls
+ * Author: Shoaib Khan
+ * Date: 27/12/2017
+ */
+
 const http = require('http');
 const URL = require('url');
+const querystring = require('querystring');
 
+const QRGenerator = require('./util/qr-generator');
+const TemplateGenerator = require('./util/templates');
+
+/**
+ * Http request handler
+ * @param {*} req 
+ * @param {*} res 
+ */
 let requestHandler = (req, res) => {
     const {headers, method, url} = req;
-    console.log(headers);
 
+    // Handle get requests
     if (method === 'GET') {
-        var parsedUrl = URL.parse(url)
-        console.log(parsedUrl);
-        res.writeHead(200, { 'Content-Type': 'text/plain'});
-        res.end(JSON.stringify(parsedUrl));    
+        var urlParams = URL.parse(url);
+        var [, format, size, text] = urlParams.path.split('/');  
+        //TODO: make the proper url with query params
+        if(format === 'png' || format === 'svg') {
+            var temp = TemplateGenerator.templates.createPngTemplate();
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(temp);
+        } else if(urlParams.pathname === '/getqr') {
+            var qs = querystring.parse(urlParams.query);
+            var [contentType, qrBuffer] = QRGenerator.process(qs.type, qs.size, qs.text);
+            res.writeHead(200, { 'Content-Type': contentType});
+            res.end(qrBuffer);
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end("Not Found");
+        }
     }
-}
+};
 
 http.createServer(requestHandler).listen(8080);
